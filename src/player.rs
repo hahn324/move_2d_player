806f130511e6b45349f3f69dfe8ad6890d81d992
Harrison Hahn <harrisonhahn96@gmail.com>
2025-11-09 15:56:28 -0500

    adds crouch

--- src/player.rs
+++ src/player.rs
@@ -5,24 +5,28 @@ use std::time::Duration;
 const PLAYER_HITBOX: (Vec2, Vec2, f32) = (Vec2::new(0.0, -30.0), Vec2::new(0.0, -12.0), 11.0);
 
 const PLAYER_SPRITE_GRID: (UVec2, u32, u32, Option<UVec2>, Option<UVec2>) =
-    (UVec2::new(110, 80), 15, 2, Some(UVec2::new(10, 0)), None);
+    (UVec2::new(110, 80), 17, 3, Some(UVec2::new(10, 0)), None);
 
 const IDLE_SPRITE_INDICES: (usize, usize) = (0, 9);
 const IDLE_SPRITE_TIMER: f32 = 0.1;
 
-const RUN_SPRITE_INDICES: (usize, usize) = (15, 24);
+const RUN_SPRITE_INDICES: (usize, usize) = (17, 26);
 const RUN_SPRITE_TIMER: f32 = 0.05;
 
-const JUMP_SQUAT_SPRITE_INDICES: (usize, usize) = (10, 11);
-const JUMP_SQUAT_TIMER: f32 = 0.05;
-
-const JUMP_SPRITE_INDEX: usize = 12;
-const JUMP_TIMER: f32 = 0.15;
+const JUMP_SPRITE_INDICES: (usize, usize) = (10, 12);
+const JUMP_TIMER: f32 = 0.18;
 
 const JUMP_FALL_SPRITE_INDICES: (usize, usize) = (13, 14);
 const JUMP_FALL_TIMER: f32 = 0.25;
 
+const CROUCH_SPRITE_INDICES: (usize, usize) = (15, 16);
+const CROUCH_TIMER: f32 = 0.1;
+
+const CROUCH_WALK_SPRITE_INDICES: (usize, usize) = (34, 41);
+const CROUCH_WALK_TIMER: f32 = 0.08;
+
 const PLAYER_SPEED: f32 = 500.0;
+const PLAYER_CROUCH_SPEED: f32 = 312.5;
 const PLAYER_DRIFT: f32 = 500.0;
 const PLAYER_JUMP_SPEED: f32 = 1000.0;
 const GRAVITY: f32 = -500.0;
@@ -39,9 +43,10 @@ pub enum PlayerState {
     #[default]
     Idle,
     Run,
-    JumpSquat,
     Jump,
     JumpFall,
+    Crouch,
+    CrouchWalk,
 }
 
 #[derive(States, Debug, Clone, Copy, Default, Eq, PartialEq, Hash)]
@@ -62,9 +67,10 @@ impl Plugin for PlayerPlugin {
             .add_systems(Startup, spawn_player)
             .add_systems(OnEnter(PlayerState::Idle), set_idle)
             .add_systems(OnEnter(PlayerState::Run), set_run)
-            .add_systems(OnEnter(PlayerState::JumpSquat), set_jump_squat)
             .add_systems(OnEnter(PlayerState::Jump), set_jump)
             .add_systems(OnEnter(PlayerState::JumpFall), set_jump_fall)
+            .add_systems(OnEnter(PlayerState::Crouch), set_sprite_crouch)
+            .add_systems(OnEnter(PlayerState::CrouchWalk), set_sprite_crouch_walk)
             .add_systems(OnEnter(PlayerDirection::Right), set_sprite_right)
             .add_systems(OnEnter(PlayerDirection::Left), set_sprite_left)
             .add_systems(
@@ -182,27 +188,12 @@ fn set_run(
     }
 }
 
-fn set_jump_squat(
-    mut query: Query<(&mut AnimationIndices, &mut AnimationTimer, &mut Sprite), With<Player>>,
-) {
-    if let Ok((mut animation_indices, mut animation_timer, mut sprite)) = query.single_mut() {
-        animation_indices.first = JUMP_SQUAT_SPRITE_INDICES.0;
-        animation_indices.last = JUMP_SQUAT_SPRITE_INDICES.1;
-
-        animation_timer.set_duration(Duration::from_secs_f32(JUMP_SQUAT_TIMER));
-
-        if let Some(atlas) = sprite.texture_atlas.as_mut() {
-            atlas.index = animation_indices.first;
-        }
-    }
-}
-
 fn set_jump(
     mut query: Query<(&mut AnimationIndices, &mut AnimationTimer, &mut Sprite), With<Player>>,
 ) {
     if let Ok((mut animation_indices, mut animation_timer, mut sprite)) = query.single_mut() {
-        animation_indices.first = JUMP_SPRITE_INDEX;
-        animation_indices.last = JUMP_SPRITE_INDEX;
+        animation_indices.first = JUMP_SPRITE_INDICES.0;
+        animation_indices.last = JUMP_SPRITE_INDICES.1;
 
         animation_timer.set_duration(Duration::from_secs_f32(JUMP_TIMER));
 
@@ -239,16 +230,46 @@ fn set_sprite_left(mut query: Query<&mut Sprite, With<Player>>) {
     }
 }
 
+fn set_sprite_crouch(
+    mut query: Query<(&mut AnimationIndices, &mut AnimationTimer, &mut Sprite), With<Player>>,
+) {
+    if let Ok((mut animation_indices, mut animation_timer, mut sprite)) = query.single_mut() {
+        animation_indices.first = CROUCH_SPRITE_INDICES.1;
+        animation_indices.last = CROUCH_SPRITE_INDICES.1;
+
+        animation_timer.set_duration(Duration::from_secs_f32(CROUCH_TIMER));
+
+        if let Some(atlas) = sprite.texture_atlas.as_mut() {
+            atlas.index = CROUCH_SPRITE_INDICES.0;
+        }
+    }
+}
+
+fn set_sprite_crouch_walk(
+    mut query: Query<(&mut AnimationIndices, &mut AnimationTimer, &mut Sprite), With<Player>>,
+) {
+    if let Ok((mut animation_indices, mut animation_timer, mut sprite)) = query.single_mut() {
+        animation_indices.first = CROUCH_WALK_SPRITE_INDICES.0;
+        animation_indices.last = CROUCH_WALK_SPRITE_INDICES.1;
+
+        animation_timer.set_duration(Duration::from_secs_f32(CROUCH_WALK_TIMER));
+
+        if let Some(atlas) = sprite.texture_atlas.as_mut() {
+            atlas.index = animation_indices.first;
+        }
+    }
+}
+
 fn update_player_drift(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     player_drift: Res<State<PlayerDrift>>,
     mut next_player_drift: ResMut<NextState<PlayerDrift>>,
 ) {
-    let pressed_right = keyboard_input.just_pressed(KeyCode::ArrowRight);
-    let released_right = keyboard_input.just_released(KeyCode::ArrowRight);
+    let pressed_right = keyboard_input.just_pressed(KeyCode::KeyD);
+    let released_right = keyboard_input.just_released(KeyCode::KeyD);
 
-    let pressed_left = keyboard_input.just_pressed(KeyCode::ArrowLeft);
-    let released_left = keyboard_input.just_released(KeyCode::ArrowLeft);
+    let pressed_left = keyboard_input.just_pressed(KeyCode::KeyA);
+    let released_left = keyboard_input.just_released(KeyCode::KeyA);
 
     match player_drift.get() {
         PlayerDrift::None => {
@@ -292,21 +313,26 @@ fn update_player_state(
     player_direction: Res<State<PlayerDirection>>,
     mut next_player_direction: ResMut<NextState<PlayerDirection>>,
 ) {
-    let pressed_right = keyboard_input.just_pressed(KeyCode::ArrowRight);
-    let released_right = keyboard_input.just_released(KeyCode::ArrowRight);
-    let right_is_pressed = keyboard_input.pressed(KeyCode::ArrowRight);
+    let pressed_right = keyboard_input.just_pressed(KeyCode::KeyD);
+    let released_right = keyboard_input.just_released(KeyCode::KeyD);
+    let right_is_pressed = keyboard_input.pressed(KeyCode::KeyD);
 
-    let pressed_left = keyboard_input.just_pressed(KeyCode::ArrowLeft);
-    let released_left = keyboard_input.just_released(KeyCode::ArrowLeft);
-    let left_is_pressed = keyboard_input.pressed(KeyCode::ArrowLeft);
+    let pressed_left = keyboard_input.just_pressed(KeyCode::KeyA);
+    let released_left = keyboard_input.just_released(KeyCode::KeyA);
+    let left_is_pressed = keyboard_input.pressed(KeyCode::KeyA);
 
     let pressed_space = keyboard_input.just_pressed(KeyCode::Space);
+
+    let pressed_control = keyboard_input.just_pressed(KeyCode::ControlLeft);
+    let released_control = keyboard_input.just_released(KeyCode::ControlLeft);
 
     match player_state.get() {
         PlayerState::Idle => {
             if pressed_space {
                 // Start jump
-                next_player_state.set(PlayerState::JumpSquat);
+                next_player_state.set(PlayerState::Jump);
+            } else if pressed_control {
+                next_player_state.set(PlayerState::Crouch);
             } else {
                 if (pressed_right && pressed_left) || (released_right && released_left) {
                     return;
@@ -328,7 +354,9 @@ fn update_player_state(
         PlayerState::Run => {
             if pressed_space {
                 // Start jump
-                next_player_state.set(PlayerState::JumpSquat);
+                next_player_state.set(PlayerState::Jump);
+            } else if pressed_control {
+                next_player_state.set(PlayerState::CrouchWalk);
             } else {
                 if released_right && pressed_left {
                     // Was Run Right, set to Run Left.
@@ -342,15 +370,6 @@ fn update_player_state(
                 } else if released_left || pressed_right {
                     // Was Run Left, set to Idle Left.
                     next_player_state.set(PlayerState::Idle);
-                }
-            }
-        }
-        PlayerState::JumpSquat => {
-            if let Ok((_, sprite, _)) = query.single() {
-                if let Some(atlas) = sprite.texture_atlas.as_ref() {
-                    if atlas.index == JUMP_SQUAT_SPRITE_INDICES.1 {
-                        next_player_state.set(PlayerState::Jump);
-                    }
                 }
             }
         }
@@ -387,6 +406,42 @@ fn update_player_state(
                 }
             }
         }
+        PlayerState::Crouch => {
+            if released_control {
+                next_player_state.set(PlayerState::Idle);
+            } else {
+                if (pressed_right && pressed_left) || (released_right && released_left) {
+                    return;
+                } else if pressed_right || released_left {
+                    next_player_state.set(PlayerState::CrouchWalk);
+                    match player_direction.get() {
+                        PlayerDirection::Left => next_player_direction.set(PlayerDirection::Right),
+                        PlayerDirection::Right => (),
+                    }
+                } else if pressed_left || released_right {
+                    next_player_state.set(PlayerState::CrouchWalk);
+                    match player_direction.get() {
+                        PlayerDirection::Right => next_player_direction.set(PlayerDirection::Left),
+                        PlayerDirection::Left => (),
+                    }
+                }
+            }
+        }
+        PlayerState::CrouchWalk => {
+            if released_control {
+                next_player_state.set(PlayerState::Run);
+            } else {
+                if released_right && pressed_left {
+                    next_player_direction.set(PlayerDirection::Left);
+                } else if released_right || pressed_left {
+                    next_player_state.set(PlayerState::Crouch);
+                } else if released_left && pressed_right {
+                    next_player_direction.set(PlayerDirection::Right);
+                } else if released_left || pressed_right {
+                    next_player_state.set(PlayerState::Crouch);
+                }
+            }
+        }
     }
 }
 
@@ -405,6 +460,10 @@ fn move_player(
                 PlayerDirection::Right => movement.x += PLAYER_SPEED,
                 PlayerDirection::Left => movement.x -= PLAYER_SPEED,
             },
+            PlayerState::CrouchWalk => match player_direction.get() {
+                PlayerDirection::Right => movement.x += PLAYER_CROUCH_SPEED,
+                PlayerDirection::Left => movement.x -= PLAYER_CROUCH_SPEED,
+            },
             PlayerState::Jump => {
                 movement.y += PLAYER_JUMP_SPEED;
                 match player_drift.get() {
@@ -413,7 +472,7 @@ fn move_player(
                     PlayerDrift::None => (),
                 }
             }
-            PlayerState::JumpSquat | PlayerState::JumpFall => match player_drift.get() {
+            PlayerState::JumpFall => match player_drift.get() {
                 PlayerDrift::Right => movement.x += PLAYER_DRIFT,
                 PlayerDrift::Left => movement.x -= PLAYER_DRIFT,
                 PlayerDrift::None => (),
